8fc55d54504169da03ed6986a800c4f4a08331ea
Olga Zaghen <olgazaghen@gmail.com>
2023-02-07 15:13:54 +0100

    for custom dataset 3

---
+++ exp/scripts/run_custom.sh
@@ -0,0 +1,16 @@
+python -m exp.run \
+    --dataset=custom \
+    --d=3 \
+    --layers=4 \
+    --hidden_channels=20 \
+    --left_weights=True \
+    --right_weights=True \
+    --lr=0.02 \
+    --weight_decay=5e-3 \
+    --input_dropout=0.0 \
+    --dropout=0.7 \
+    --use_act=True \
+    --model=BundleSheaf \
+    --normalised=True \
+    --sparse_learner=True \
+    --entity="${ENTITY}"
